6cb52ca4ec5aaa8e17c2e4158014e4050f51b497
Eshei Jenkins <eshjenk@gmail.com>
2024-10-15 14:14:24 -0400

    functions

---
+++ functions/more-functions.js
@@ -0,0 +1,11 @@
+"use script";
+function displayMailingLabel(name, address, city, state) {
+  console.log(name, address, city, state);
+}
+
+displayMailingLabel("Eshei Jenkins", "5 Cherry Street", "Stamford", "CT", "06410");
+
+function addNumbers(num1, num2) {
+  console.log(num1 + "+" + num2 + "=" + (num1 + num2));
+}
+addNumbers(2, 2);
---
+++ functions/simple-functions.js
@@ -0,0 +1,20 @@
+function favoriteThings() {
+  console.log("Eshei");
+  console.log("Love Jones");
+  console.log("Todd Galberth");
+}
+
+favoriteThings();
+
+function whyImHere() {
+  console.log(
+    "I joined YearUp to better myself and my future. I want to work in the tech space so that I can have a career, not a job that offers financial stability and work-life balance."
+  );
+}
+whyImHere();
+
+function favoritePlace() {
+  console.log("One of my favorite places to visit has been Jamaica. I enjoy the tropical weather and their food!");
+}
+
+favoritePlace();
