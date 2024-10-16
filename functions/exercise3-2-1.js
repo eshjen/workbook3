1f1ec5541fb0480a19bdecc03c39fa546b5f14d5
Eshei Jenkins <eshjenk@gmail.com>
2024-10-16 11:25:15 -0400

    exercise 3

---
+++ functions/exercise3-2-1.js
@@ -0,0 +1,10 @@
+"use strict";
+function isEven(num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+console.log(isEven(8));
+console.log(isEven(3));
--- functions/more-functions.js
+++ functions/more-functions.js
@@ -4,13 +4,20 @@ function displayMailingLabel(name, address, city, state) {
 }
 
 displayMailingLabel("Eshei Jenkins", "5 Cherry Street", "Stamford", "CT", "06410");
+displayMailingLabel("Kayli Moore", "12 Francis Street", "Schenectady", "NY", "12301");
+displayMailingLabel("Victoria Canteen", "3328 Westchester Blvd", "Bronx", "NY", "10465");
 
 function addNumbers(num1, num2) {
   console.log(num1 + "+" + num2 + "=" + (num1 + num2));
 }
 addNumbers(2, 2);
+addNumbers(16, 18);
+addNumbers(98, 98);
 
 function displayReceipt(totalDue, amountPaid) {
   console.log("Total Due: " + totalDue + " Amount Paid: " + amountPaid + " Change Due: " + (amountPaid - totalDue));
 }
-displayReceipt(65,80)
+displayReceipt(65, 80);
+displayReceipt(30,50
+displayReceipt(24,24)
+displayReceipt(75,60)
