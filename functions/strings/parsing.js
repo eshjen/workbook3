3405e3227482610fdaa8bf3d4aa5ab020dd786c8
Eshei Jenkins <eshjenk@gmail.com>
2024-10-17 09:39:44 -0400

    adding another name

--- functions/strings/parsing.js
+++ functions/strings/parsing.js
@@ -4,10 +4,13 @@ function parseAndDisplayName(fullName){
     
 let space = fullName.indexOf(" ");
 let firstName = fullName.substring(0, space);
-let lastName = fullName.substring(space+1, 8)
+let lastName = fullName.substring(space+1,)
 console.log(space);
 
 console.log("Name:", fullName);
 console.log("First name: ", firstName);
 console.log("Last name: ", lastName)
 }
+
+parseAndDisplayName("Brenda Kaye")
+parseAndDisplayName("Victoria Terrell")
